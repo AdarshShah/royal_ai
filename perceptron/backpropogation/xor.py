4c20c086a549d7263e6f0891cfedb4351319960f
Adarsh Shah <adashah21998@gmail.com>
2019-11-08 07:13:07 +0530

    xor updated

--- perceptron/backpropogation/xor.py
+++ perceptron/backpropogation/xor.py
@@ -5,54 +5,55 @@ output = np.array([[0],[1],[1],[0]])
 
 class NeuralNetwork(object):
 
-    def __init__(self):
-        self.W_1 = np.random.rand(2,2)
-        self.b_1 = np.random.rand(1,2)
+    def __init__(self,input,hidden,output):
+        self.input = input
+        self.hidden = hidden
+        self.output = output
 
-        self.W_2 = np.random.rand(2,1)
-        self.b_2 = np.random.rand(1,1)
-
+        self.W1 = np.random.rand(self.input,self.hidden)
+        self.b1 = np.random.rand(1,self.hidden)
+        self.W2 = np.random.rand(self.hidden,self.output)
+        self.b2 = np.random.rand(1,self.output)
+    
     def sigmoid(self,a):
         return 1/(1+np.exp(-a))
 
+    def sigmoid_deriv(self,a):
+        return a*(1-a)
+
     def feedforward(self,input):
-        self.a_1 = np.dot(input,self.W_1)+self.b_1
-        self.h_1 = self.sigmoid(self.a_1)
+        self.a1 = np.dot(input,self.W1) + self.b1
+        self.h1 = self.sigmoid(self.a1)
+        self.a2 = np.dot(self.h1,self.W2) + self.b2
+        self.h2 = self.sigmoid(self.a2)
+        return self.h2
 
-        self.a_2 = np.dot(input,self.W_2)+self.b_2
-        self.h_2 = self.sigmoid(self.a_2)
-
-        return self.h_2
-
-    def backprop(self,input,output):
+    def backprop(self,input,output,eta):
         self.feedforward(input)
-        d_error_h_2 = self.h_2 - output
-        d_error_a_2 = d_error_h_2 * (1-self.h_2) * self.h_2 
+        error = self.h2 - output
+        d_error_a2 = error*self.sigmoid_deriv(self.h2)
 
-        d_error_h_1 = self.W_2.T*d_error_a_2
-        d_error_a_1 = d_error_h_1 * self.h_1 * (1-self.h_1)
+        d_error_W2 = self.h1.T.dot(d_error_a2)
+        d_error_b2 = np.sum(d_error_a2,axis=0)
+        d_error_a1 = d_error_a2 * self.W2.T * self.sigmoid_deriv(self.h1)
+
+        d_error_W1 = input.T.dot(d_error_a1)
+        d_error_b1 = np.sum(d_error_a1,axis=0)
+
+        self.W1 -= eta*d_error_W1
+        self.b1 -= eta*d_error_b1
+        self.W2 -= eta*d_error_W2
+        self.b2 -= eta*d_error_b2
         
-        self.d_error_W_2 = self.h_1.T.dot(d_error_a_2)
-        self.d_error_b_2 = d_error_a_2
-        
-        self.d_error_W_1 = input.T.dot(d_error_a_1)
-        self.d_error_b_1 = d_error_a_1
 
-    def train(self,iter,eta,input,output):
-        for _ in range(iter):
-            for i,o in zip(input,output):
-                self.feedforward(i)
-                self.backprop(np.array([i]),o)
-                self.W_1-=eta*self.d_error_W_1
-                self.b_1-=eta*self.d_error_b_1
-                self.W_2-=eta*self.d_error_W_2
-                self.b_2-=eta*self.d_error_b_2
-            if _%100 == 0:
-                print(np.sum((self.h_2-output)*(self.h_2-output)))
+n = NeuralNetwork(2,2,1)
+for i in range(1000):
+    n.backprop(input,output,1)
+print(n.feedforward(input))       
 
-n = NeuralNetwork()
-n.train(1000,2,input,output)
-for i,o in zip(input,output):
-    print(n.feedforward(i))
-
-
+input = np.array([[0,0,0],[0,0,1],[0,1,0],[0,1,1],[1,0,0],[1,0,1],[1,1,0],[1,1,1]])
+output = np.array([[0],[1],[1],[0],[1],[0],[0],[1]])
+n = NeuralNetwork(3,4,1)
+for i in range(2000):
+    n.backprop(input,output,1)
+print(n.feedforward(input))       
